404d17e22a28d18c9cd46c6869152027bc639f95
kamilx19 <68401816+kamilx19@users.noreply.github.com>
2020-11-15 14:56:29 +0100

    Update Odczyt.cs

--- Gra Snake/Odczyt.cs
+++ Gra Snake/Odczyt.cs
@@ -14,6 +14,7 @@ namespace Gra_Snake
         /// <param name="CzyWcisniety">Czy przycisk jest wciśnięty.</param>
         /// <param name="ZmienStatus">Zmiana statusu.</param>
         /// </summary>
+        ///</returns></returns>
         
     class Odczyt
     {
